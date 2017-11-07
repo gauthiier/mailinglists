5ff97f3a027bd2933ae0eeae808422116ded9800
gauthiier <d@gauthiier.info>
2017-11-07 14:17:31 +0100

    debug double loading

--- www/archives.py
+++ www/archives.py
@@ -5,14 +5,12 @@ class Singleton(type):
     def __call__(cls, *args, **kwargs):
         if cls not in cls._instances:
             cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
+            logging.info('**** new Singleton instance')
         return cls._instances[cls]
 
 class Archives(metaclass=Singleton):
 
 	def __init__(self, archives_dir=None):
-
-
-		print("Archives::init")
 
 		if archives_dir==None:
 			from www import config
